393f62c49fc7ad4fc8ef029652c641b433438f2e
Will Queree <wqueree@gmail.com>
2022-03-13 15:27:10 +0000

    Generate CSV of all 5-letter words

---
+++ generate_fives.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from typing import List
+import pandas as pd
+
+if __name__ == "__main__":
+    words_txt: Path = Path("words.txt")
+    fives: List[str] = []
+    with open(words_txt, encoding="utf-8") as words_file:
+        line: str = words_file.readline().strip()
+        while line:
+            if len(line) == 5:
+                fives.append(line.upper())
+            line: str = words_file.readline().strip()
+    fives_series: pd.Series = pd.Series(fives)
+    fives_series.to_csv("fives.csv")
